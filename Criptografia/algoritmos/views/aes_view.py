1c80a98630efe000cfa687c9176adc1cddb7fdc1
aideebibi <aideebibi@users.noreply.github.com>
2021-07-14 02:46:28 -0500

    Descifrado aes ecb

--- Criptografia/algoritmos/views/aes_view.py
+++ Criptografia/algoritmos/views/aes_view.py
@@ -13,29 +13,22 @@ class AES_EBC(View):
 
     # Pueden ver a la función "get" como un main
     def get(self, request, *args, **kwargs):  
-        tiemposCifrado = []
-        tiemposDescifrado = []
-        listaLlaves = []
-        listaMensajes = []
+        listaLlaves = listaMensajes = []
+        listaBytesCifrados = listaBytesDescifrados = []
+        tiemposCifrado = tiemposDescifrado = []
+        
         archivo_lectura = settings.MEDIA_ROOT + "/AES_EBC.rsp"
 
         listaLlaves, listaMensajes = self.leer_archivo(archivo_lectura, listaLlaves, listaMensajes)
-        print("\n\n\n================================")
-        print("Lista llaves: " + str(listaLlaves))
-        print("\n\n\n================================")
-        print("Lista mensajes: " + str(listaMensajes))
-        print("\n\n\n")
 
-        self.cifrado_aes_ecb(listaLlaves, listaMensajes, tiemposCifrado)
+        self.cifrado_aes_ecb(listaLlaves, listaMensajes, tiemposCifrado, listaBytesCifrados)
+        self.descifrado_aes_ecb(listaLlaves, listaMensajes, tiemposDescifrado, listaBytesCifrados, listaBytesDescifrados)
 
         context = {
-            # Nota: El contexto son las variables que se van a mandar a la página web
-            # Si quieren por el momento no vamos a mandar nada a la página web y todo lo manejamos por la consola
-            # Y a variables que decidan imprimir al final, son las que se mandarán en el conexto a la pg web
-            # Por el momento lo pueden dejar en blanco
-
-            # La estructura es la siguiente
-            # 'nombre_variable_html' : nombre_variable_python,
+            'listaTiemposCifrado' : tiemposCifrado,
+            'listaTtiemposDescifrado' : tiemposDescifrado,
+            'listaBytesCifrados' : listaBytesCifrados,
+            'listaBytesDescifrados' : listaBytesDescifrados,
         }
         return render(request, self.template_name, context)
     
@@ -55,10 +48,8 @@ class AES_EBC(View):
         archivo_lectura.close()
         return listaLlaves, listaMensajes
     
-    def cifrado_aes_ecb(self, listaLlaves, listaMensajes, tiemposCifrado):
-        tamanioListaLlaves = len(listaLlaves)
+    def cifrado_aes_ecb(self, listaLlaves, listaMensajes, tiemposCifrado, listaBytesCifrados):
         tamanioListaMensajes = len(listaMensajes)
-
         for i in range(0, tamanioListaMensajes):
             #Conversión de elementos
             mensajeConcatenado = mensaje = bytearray.fromhex(listaMensajes[i]) 
@@ -71,10 +62,29 @@ class AES_EBC(View):
             
             objetoCifrar = AES.new(llave, AES.MODE_ECB)
             tiempoInicio = time()
-            bytesCifrados = objetoCifrar.encrypt(mensajeConcatenado)
+            listaBytesCifrados.append(objetoCifrar.encrypt(mensajeConcatenado))
             tiempoFinal = time ()
             tiemposCifrado.append(tiempoFinal - tiempoInicio)
             #print("\nBytes Cifrados: ", bytesCifrados.hex())
+        return
+    
 
-        #print("Tamanio llaves: " + str(len(listaLlaves)))
-        #print("Tamanio mensajes: " + str(len(listaMensajes)))
+    def descifrado_aes_ecb(self, listaLlaves, listaMensajes, tiemposDescifrado, listaBytesCifrados):
+        tamanioListaMensajes = len(listaMensajes)
+        for i in range(0, tamanioListaMensajes):
+            #Conversión de elementos
+            mensajeConcatenado = mensaje = listaBytesCifrados[i]
+            llave = bytearray.fromhex(listaLlaves[i])
+            
+            #Concatenamos el mensaje 
+            for j in range(0, 111):
+                mensajeConcatenado = mensajeConcatenado + mensaje
+            #print("\nMENSAJE = ", mensajeConcatenado)
+            
+            objetoDescifrar = AES.new(llave, AES.MODE_ECB)
+            tiempoInicio = time()
+            listaBytesCifrados.append(objetoDescifrar.decrypt(mensajeConcatenado))
+            tiempoFinal = time ()
+            tiemposDescifrado.append(tiempoFinal - tiempoInicio)
+            #print("\nBytes Descifrados: ", bytesDescifrados.hex())
+        return
